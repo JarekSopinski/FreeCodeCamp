f4c280aaf28a638def71765234337afe53cc1aef
JarekSopinski <jarek.sopinski@gmail.com>
2018-03-01 16:02:24 +0100

    solution for 'Boo who' algorithm challenge

---
+++ IntermediateAlgorithmScripting/08_Missing_letters.js
@@ -0,0 +1,10 @@
+/*
+INSTRUCTIONS FROM FCC:
+Find the missing letter in the passed letter range and return it.
+If all letters are present in the range, return undefined.
+
+fearNotLetter("abce") should return "d".
+fearNotLetter("abcdefghjklmno") should return "i".
+fearNotLetter("bcd") should return undefined.
+fearNotLetter("yz") should return undefined.
+ */
